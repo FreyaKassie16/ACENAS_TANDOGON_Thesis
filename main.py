905cfa13437a6d0db75b653b41eb794fc06e0664
FreyaKassie16 <shaunmarvin.acenas@gmail.com>
2024-10-05 22:00:37 +0800

    Accidentally put download_dataset code in main.py, fixed it

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from ultralytics import YOLO
 
-from roboflow import Roboflow
-rf = Roboflow(api_key="d84vxl7g4KPneFkzL5GG")
-project = rf.workspace("roboflow-universe-projects").project("basketball-players-fy4c2")
-version = project.version(16)
-dataset = version.download("yolov11")
-                
-
 model = YOLO('yolo11n.pt')
 
 results = model.train(data="dataset/data.yaml", epochs=100, imgsz=640)
